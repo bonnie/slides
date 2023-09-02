364448f5077daa0c17c647085237bdd193e4265a
Bonnie Schulkin <biz@bonnieschulkin.com>
2023-09-02 12:30:51 -0700

    Fix caption styling

--- slidev/2023-09-25-ijs-rsc/bonniedotdev/components/Caption.vue
+++ slidev/2023-09-25-ijs-rsc/bonniedotdev/components/Caption.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
 import { computed } from "vue";
 
-const className = computed(() => {
-  const direction = $slidev.nav.currentLayout.startsWith("image-left")
-    ? "right"
-    : "left";
-  return `fixed bottom-0 ${direction}-[50%] width-[50vw] p-4 text-white text-sm bg-gray-800/75`;
+const style = computed(() => {
+  return $slidev.nav.currentLayout.startsWith("image-left")
+    ? { right: "50%" }
+    : { left: "50%" };
 });
 </script>
 
 <template>
-  <div :class="className">
+  <div
+    class="fixed bottom-0 p-4 text-white text-sm bg-gray-800/75"
+    :style="style"
+  >
     <slot />
   </div>
 </template>
